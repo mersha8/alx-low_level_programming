c604af20a516bc8521798797fc2a33864c59646c
mersha8 <mershaelias8@gmail.com>
2022-07-07 02:34:50 -0700

    low level programming

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,35 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-int _putchar(char c);
 
 /**
- * print_number - like a hello world
- *
- * @n: params an integer
- *
- * No return
- */
-
-
+* print_number - prints # using _putchar function
+* @n: the integer to print
+*
+* Return: void
+*/
 void print_number(int n)
 {
+	int copy, nth, size = 1, ones = n % 10;
 
-	unsigned int n1 = 0;
-
-	if  (n < 0)
+	n /= 10;
+	copy = n;
+	if (ones < 0)
 	{
-		n1 = -n;
+		ones *= -1, copy *= -1, n *= -1;
 		_putchar('-');
 	}
-
-	else
+	if (copy > 0)
 	{
-		n1 = n;
+		while (copy / 10 != 0)
+		{
+			copy /= 10, size *= 10;
+		}
+		while (size > 0)
+		{
+			nth = n / size;
+			_putchar('0' + nth);
+			n -= nth * size;
+			size /= 10;
+		}
 	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
+	_putchar('0' + ones);
 }
