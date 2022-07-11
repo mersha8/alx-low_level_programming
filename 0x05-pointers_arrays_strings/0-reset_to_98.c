cf796562ff6b6a1553c031aa3b9034700de9b482
mersha8 <mershaelias8@gmail.com>
2022-07-11 06:24:05 -0700

    alx-low_level_programming

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * reset_to_98 - updates the value *n points to.
- * @n: the number to modify.
- */
-
+* reset_to_98 -a function that takes a pointer to an int
+* @n: puntero
+* return (0)
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
